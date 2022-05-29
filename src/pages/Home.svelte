b5c0ec5bd7d36df7c9eaa14bbd76c94a22430323
Kamil Adamiec <kmlkamilek@gmail.com>
2022-05-29 20:31:33 +0200

    Exercise list page #3 (#7)

---
+++ src/pages/Home.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { push } from "svelte-spa-router";
+    import Button, { Label } from "@smui/button";
+</script>
+
+<Button variant="unelevated" on:click={() => push("/exercises")}>
+    <Label>Exercise List</Label>
+</Button>
