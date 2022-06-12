7340016aa47e73e9932986d28e73f5cdf3c7d0da
Kamil Adamiec <kmlkamilek@gmail.com>
2022-06-12 20:24:34 +0200

    Exercise edition #5 (#14)

--- src/pages/Exercises.svelte
+++ src/pages/Exercises.svelte
@@ -1,17 +1,8 @@
 <script lang="ts">
     import { push } from "svelte-spa-router";
 
-    function goToExerciseEdit(exerciseId) {
-        push(`/exercises/${exerciseId}/edit`);
-    }
-
     function goToExerciseDetails(exerciseId) {
         push(`/exercises/${exerciseId}/details`)
-    }
-
-    function openModal() {
-        let modalElement = document.querySelector("#app-modal");
-        modalElement.classList.add("modal-open");
     }
 
     // TODO: Fetch exercise list from backend
@@ -29,12 +20,33 @@
         return Math.max.apply(null, idsArray);
     }
 
-    function closeModal() {
-        let modalElement = document.querySelector("#app-modal");
+    function openExerciseAddModal() {
+        let modalElement = document.querySelector("#exercise-add-modal");
+        modalElement.classList.add("modal-open");
+    }
+
+    function openExerciseEditModal(exerciseId: number) {
+        let nameElement: HTMLInputElement = document.querySelector("input#edit-name");
+        nameElement.value = exercises.find((exercise) => exercise.id == exerciseId).name;
+
+        let idElement: HTMLInputElement = document.querySelector("input#edit-id");
+        idElement.value = exerciseId.toString();
+
+        let modalElement = document.querySelector("#exercise-edit-modal");
+        modalElement.classList.add("modal-open");
+    }
+
+    function closeExerciseAddModal() {
+        let modalElement = document.querySelector("#exercise-add-modal");
         modalElement.classList.remove("modal-open");
     }
 
-    function handleModalOk() {
+    function closeExerciseEditModal() {
+        let modalElement = document.querySelector("#exercise-edit-modal");
+        modalElement.classList.remove("modal-open");
+    }
+
+    function handleExerciseAddModalOk() {
         let nameElement: HTMLInputElement = document.querySelector("input#name");
         if(nameElement.value == '' || nameElement.value == undefined) {
             let nameErrorElement: HTMLSpanElement = document.querySelector("span#name-error");
@@ -45,7 +57,24 @@
         let exerciseName: string = nameElement.value;
         let highestId: number = resolveHighestExerciseId() + 1;
         exercises = [...exercises, {id: highestId, name: exerciseName}]
-        closeModal();
+        closeExerciseAddModal();
+    }
+
+    function handleExerciseEditModalOk() {
+        let nameElement: HTMLInputElement = document.querySelector("input#edit-name");
+        if(nameElement.value == '' || nameElement.value == undefined) {
+            let nameErrorElement: HTMLSpanElement = document.querySelector("span#name-error");
+            nameErrorElement.innerText = "Name cannot be empty";
+            return;
+        }
+
+        let idElement: HTMLInputElement = document.querySelector("input#edit-id");
+
+        let exerciseName = nameElement.value;
+        let exerciseId = parseInt(idElement.value);
+        exercises = [...exercises.filter((exercise) => exercise.id != exerciseId), {id: exerciseId, name: exerciseName}]
+            .sort((exerciseA, exerciseB) => exerciseA.id - exerciseB.id);
+        closeExerciseEditModal();
     }
 </script>
 
@@ -56,7 +85,7 @@
             <th class="w-14">
             </th>
             <th class="w-14">
-                <button class="btn btn-primary btn-sm" on:click={() => openModal()}>Add</button>
+                <button class="btn btn-primary btn-sm" on:click={() => openExerciseAddModal()}>Add</button>
             </th>
         </tr>
     </thead>
@@ -65,7 +94,7 @@
             <tr>
                 <td>{name}</td>
                 <td colspan="2" class="w-14">
-                    <button class="btn btn-primary btn-xs" on:click={() => goToExerciseEdit(id)}>Edit</button>
+                    <button class="btn btn-primary btn-xs" on:click={() => openExerciseEditModal(id)}>Edit</button>
                     <button class="btn btn-primary btn-xs" on:click={() => goToExerciseDetails(id)}>Details</button>
                 </td>
             </tr>
@@ -73,7 +102,7 @@
     </tbody>
 </table>
 
-<div id="app-modal" class="modal">
+<div id="exercise-add-modal" class="modal">
     <div class="modal-box w-11/12 max-w-5xl">
         <div class="form-control w-full max-w-xs">
             <label for="name" class="label">
@@ -84,17 +113,33 @@
                 <span id="name-error" class="label-text-alt text-red-600"></span>
             </label>
         </div>
-        <div class="form control w-full max-w-xs">
-            <label for="description" class="label">
-                <span class="label-text">Exercise description</span>
+        <div class="flex">
+            <div class="grow"></div>
+            <div class="flex-none modal-action">
+                <button class="btn btn-success" on:click={() => handleExerciseAddModalOk()}>Ok</button>
+                <button class="btn btn-error" on:click={() => closeExerciseAddModal()}>Cancel</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="exercise-edit-modal" class="modal">
+    <div class="modal-box w-11/12 max-w-5xl">
+        <input type="hidden" id="edit-id"/>
+        <div class="form-control w-full max-w-xs">
+            <label for="edit-name" class="label">
+                <span class="label-text">Exercise name</span>
             </label>
-            <textarea id="description" class="textarea textarea-primary w-full max-w-xs" placeholder="Description"></textarea>
+            <input type="text" placeholder="Name" id="edit-name" class="input input-bordered input-primary w-full max-w-xs"/>
+            <label for="edit-name" class="label">
+                <span id="edit-name-error" class="label-text-alt text-red-600"></span>
+            </label>
         </div>
         <div class="flex">
             <div class="grow"></div>
             <div class="flex-none modal-action">
-                <label for="app-modal" class="btn btn-success" on:click={() => handleModalOk()}>Ok</label>
-                <label for="app-modal" class="btn btn-error" on:click={() => closeModal()}>Cancel</label>
+                <button class="btn btn-success" on:click={() => handleExerciseEditModalOk()}>Ok</button>
+                <button class="btn btn-error" on:click={() => closeExerciseEditModal()}>Cancel</button>
             </div>
         </div>
     </div>
