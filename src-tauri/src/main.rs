76a58ce83b6636f5abd9d4fff82e63469b2c9b63
Kamil Adamiec <kmlkamilek@gmail.com>
2022-06-29 14:57:22 +0200

    Add logging library to application #21 (#57)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -12,21 +12,26 @@ mod payloads;
 use db::*;
 use db::models::*;
 use payloads::*;
+use log::info;
+use simple_logger::SimpleLogger;
 
 #[tauri::command]
 fn get_exercises(db_state: tauri::State<DatabaseState>) -> Vec<Exercise> {
+    info!("Invoking get_exercises command");
     let db = db_state.get_db();
     return db.find_exercises();
 }
 
 #[tauri::command]
 fn get_exercise(db_state: tauri::State<DatabaseState>, id: String) -> Exercise {
+    info!("Invoking get_exercise command with parameters: {{ id: {} }}", id);
     let db = db_state.get_db();
     return db.find_exercise(id);
 }
 
 #[tauri::command]
 fn add_exercise(db_state: tauri::State<DatabaseState>, exercise: ExercisePayload) {
+    info!("Invoking add_exercise command with parameters: {{ exercise: {:?} }}", exercise);
     let new_exercise = NewExercise{
         name: match &exercise.name {
             Some(x) => x.as_str(),
@@ -47,6 +52,7 @@ fn add_exercise(db_state: tauri::State<DatabaseState>, exercise: ExercisePayload
 
 #[tauri::command]
 fn update_exercise(db_state: tauri::State<DatabaseState>, id: String, exercise: ExercisePayload) {
+    info!("Invoking update_exercise command with parameters: {{ id: {}, exercise: {:?} }}", id, exercise);
     let update_exercise = UpdateExercise {
         name: match &exercise.name {
             Some(x) => Some(x.as_str()),
@@ -63,6 +69,8 @@ fn update_exercise(db_state: tauri::State<DatabaseState>, id: String, exercise: 
 }
 
 fn main() {
+    SimpleLogger::new().init().unwrap();
+    info!("Running application");
     tauri::Builder::default()
         .manage(DatabaseState::new(Database::new()))
         .invoke_handler(tauri::generate_handler![get_exercises, get_exercise, add_exercise, update_exercise])
