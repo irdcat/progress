72a96656628973c9c599e4c8cf50277d02560826
irdcat <kmlkamilek@gmail.com>
2022-07-03 13:27:24 +0200

    Cleanup of models #74

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,12 +7,10 @@
 extern crate rusqlite;
 
 mod db;
-mod payloads;
 
 use db::*;
 use db::models::*;
-use payloads::*;
-use log::info;
+use log::{info, error};
 use simple_logger::SimpleLogger;
 
 #[tauri::command]
@@ -30,50 +28,28 @@ fn get_exercise(db_state: tauri::State<DatabaseState>, id: String) -> Exercise {
 }
 
 #[tauri::command]
-fn add_exercise(db_state: tauri::State<DatabaseState>, exercise: ExercisePayload) {
+fn add_exercise(db_state: tauri::State<DatabaseState>, exercise: ExercisePatch) {
     info!("Invoking add_exercise command with parameters: {{ exercise: {:?} }}", exercise);
-    let new_exercise = NewExercise{
-        name: match &exercise.name {
-            Some(x) => x.to_string(),
-            None => panic!("Name required for new exercise"),
-        },
-        description: match &exercise.description {
-            Some(x) => Some(x.to_string()),
-            None => None,
-        },
-        bodyweight: match exercise.bodyweight {
-            Some(x) => x,
-            None => false,
-        }
-    };
     let db = db_state.get_db();
-    db.add_exercise(new_exercise);
+    db.add_exercise(exercise);
 }
 
 #[tauri::command]
-fn update_exercise(db_state: tauri::State<DatabaseState>, id: String, exercise: ExercisePayload) {
+fn update_exercise(db_state: tauri::State<DatabaseState>, id: String, exercise: ExercisePatch) {
     info!("Invoking update_exercise command with parameters: {{ id: {}, exercise: {:?} }}", id, exercise);
-    let update_exercise = UpdateExercise {
-        name: match &exercise.name {
-            Some(x) => Some(x.to_string()),
-            None => None,
-        },
-        description: match &exercise.description {
-            Some(x) => Some(x.to_string()),
-            None => None,
-        },
-        bodyweight: exercise.bodyweight,
-    };
     let db = db_state.get_db();
-    db.update_exercise(id, update_exercise);
+    db.update_exercise(id, exercise);
 }
 
 fn main() {
     SimpleLogger::new().init().unwrap();
     info!("Running application");
-    tauri::Builder::default()
+    let result = tauri::Builder::default()
         .manage(DatabaseState::new(Database::new()))
         .invoke_handler(tauri::generate_handler![get_exercises, get_exercise, add_exercise, update_exercise])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .run(tauri::generate_context!());
+    match result {
+        Ok(_) => (),
+        Err(err) => error!("Application run failed: {}", err),
+    }
 }
--- src-tauri/src/payloads.rs
+++
@@ -1,8 +0,0 @@
-use serde::Deserialize;
-
-#[derive(Deserialize, Debug)]
-pub struct ExercisePayload {
-    pub name: Option<String>,
-    pub description: Option<String>,
-    pub bodyweight: Option<bool>,
-}
