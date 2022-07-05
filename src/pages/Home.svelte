7a17cb4bdbb90caa553777eabd4b24751c3e5c51
Kamil Adamiec <kmlkamilek@gmail.com>
2022-07-06 01:11:56 +0200

    Home page changes #65 (#85)

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -2,5 +2,13 @@
     import { push } from "svelte-spa-router";
 </script>
 
-<button class="btn" on:click={() => push("/exercises")}>Exercises</button>
-<button class="btn" on:click={() => push("/trainings")}>Trainings</button>
+<div class="hero min-h-full bg-base-300">
+    <div class="hero-content text-center">
+        <div class="max-w-md">
+            <h1 class="text-5xl font-bold pb-8">Progress</h1>
+            <button class="btn" on:click={() => push("/exercises")}>Exercises</button>
+            <button class="btn" on:click={() => push("/trainings")}>Trainings</button>
+        </div>
+    </div>
+</div>
+
